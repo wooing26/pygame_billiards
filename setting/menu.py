31cf427001328d84ba4d9f51e950b69b7bb29fe8
wooing26 <104512612+wooing26@users.noreply.github.com>
2022-12-23 22:43:02 +0900

    .

--- setting/menu.py
+++ setting/menu.py
@@ -1,25 +1,33 @@
 import pygame
-from setting.config import *
+#from setting.config import *
+from config import *
 
 
+pygame.init()
 
 def Main_menu(menu, play):
+    global back_size
+    font6 = pygame.font.SysFont('notosansmonocjkkrregular',100)
+    img6 = font6.render('Play',True,blue)
+    mid = np.array([back_size[0] / 2, back_size[1] / 2])
     
     while menu:
-        
+        mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 menu = False
-                play = True
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_down = pygame.mouse.get_pos()
-            
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_up = pygame.mouse.get_pos()
+        
         background.fill(black)
+        #if mouse_pos
+        background.blit(img6, mid)
         pygame.display.update()
 
     return play
     
-
+Main_menu(menu=True, play=False)
+pygame.quit()
 
    
 
