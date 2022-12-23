b79a4c19a3755bc3ada4ef1c7d2bbdc0937d731c
wooing26 <104512612+wooing26@users.noreply.github.com>
2022-12-23 16:17:47 +0900

    seperate

---
+++ spin.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pygame
+from config import *
+
+# 스핀용 공 설정 (UI)
+spin_ball = [x_out[0] + 150, y_out[0] - 200]
+spin_radius = 150
+
+# 스핀 입력
+def Get_spin(mouse_pos, v_cm):
+    r_impact = [0, 0, 0]
+    v_cm = v_cm / np.linalg.norm(v_cm)
+    r_hat = [mouse_pos[0] - spin_ball[0], mouse_pos[1] - spin_ball[1]]
+    r_impact[2] = r_hat[1]
+    r_impact[0], r_impact[1] = -r_hat[0] * v_cm[1], r_hat[0] * v_cm[0]
+    return np.array(r_impact) #r_impact는 큰 값
